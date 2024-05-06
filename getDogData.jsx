1e9f6bec0b861535a829c8bf583b98b17b8223fc
archae13 <archae13@lpbroadband,net>
2024-05-06 16:58:29 -0600

    getting basic data back

--- getDogData.jsx
+++ getDogData.jsx
@@ -1,32 +1,3 @@
-const Pagination = ({ items, pageSize, onPageChange }) => {
-  const { Button } = ReactBootstrap;
-  if (items.length <= 1) return null;
-
-  let num = Math.ceil(items.length / pageSize);
-  let pages = range(1, num + 1);
-  const list = pages.map((page) => {
-    return (
-      <Button key={page} onClick={onPageChange} className="page-item">
-        {page}
-      </Button>
-    );
-  });
-  return (
-    <nav>
-      <ul className="pagination">{list}</ul>
-    </nav>
-  );
-};
-const range = (start, end) => {
-  return Array(end - start + 1)
-    .fill(0)
-    .map((item, i) => start + i);
-};
-function paginate(items, pageNumber, pageSize) {
-  const start = (pageNumber - 1) * pageSize;
-  let page = items.slice(start, start + pageSize);
-  return page;
-}
 const useDataApi = (initialUrl, initialData) => {
   const { useState, useEffect, useReducer } = React;
   const [url, setUrl] = useState(initialUrl);
@@ -44,7 +15,10 @@ const useDataApi = (initialUrl, initialData) => {
       try {
         const result = await axios(url);
         if (!didCancel) {
-          dispatch({ type: "FETCH_SUCCESS", payload: result.data });
+          let dogArray = Object.keys(result.data.message);
+          console.log(`result ${JSON.stringify(dogArray)}`);
+
+          dispatch({ type: "FETCH_SUCCESS", payload: dogArray });
         }
       } catch (error) {
         if (!didCancel) {
@@ -87,57 +61,18 @@ const dataFetchReducer = (state, action) => {
 // Get the list of Dog Breeds
 function App() {
   const { Fragment, useState, useEffect, useReducer } = React;
-  const [query, setQuery] = useState("MIT");
-  const [currentPage, setCurrentPage] = useState(1);
-  const pageSize = 10;
   const [{ data, isLoading, isError }, doFetch] = useDataApi(
-    "https://hn.algolia.com/api/v1/search?query=MIT",
+    "https://dog.ceo/api/breeds/list/all",
     {
-      hits: [],
+      message: [],
     }
   );
-  const handlePageChange = (e) => {
-    setCurrentPage(Number(e.target.textContent));
-  };
-  let page = data.hits;
-  if (page.length >= 1) {
-    page = paginate(page, currentPage, pageSize);
-    console.log(`currentPage: ${currentPage}`);
-  }
+
   return (
     <Fragment>
-      <form
-        onSubmit={(event) => {
-          doFetch("https://dog.ceo/api/breeds/list/all");
-          event.preventDefault();
-        }}
-      >
-        <input
-          type="text"
-          value={query}
-          onChange={(event) => setQuery(event.target.value)}
-        />
-        <button type="submit">Search</button>
-      </form>
-
       {isError && <div>Something went wrong ...</div>}
 
-      {isLoading ? (
-        <div>Loading ...</div>
-      ) : (
-        <ul>
-          {page.map((item) => (
-            <li key={item.objectID}>
-              <a href={item.url}>{item.title}</a>
-            </li>
-          ))}
-        </ul>
-      )}
-      <Pagination
-        items={data.hits}
-        pageSize={pageSize}
-        onPageChange={handlePageChange}
-      ></Pagination>
+      {isLoading ? <div>Loading ...</div> : <p>{JSON.stringify(data)}</p>}
     </Fragment>
   );
 }
